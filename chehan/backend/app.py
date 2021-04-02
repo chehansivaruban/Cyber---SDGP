beb68c44a9a2056c8310e73d19cef8b73c66ec22
chehansivaruban <63904379+chehansivaruban@users.noreply.github.com>
2021-04-02 16:24:12 +0530

    dummy backend

--- chehan/backend/app.py
+++ chehan/backend/app.py
@@ -1,16 +1,31 @@
 from flask import Flask, request, jsonify
+import json
+# from flask import Blueprint
+# from flask_restful import Api
+# from resources.Hello import Hello
+#
+# api_bp = Blueprint('api', __name__)
+# api = Api(api_bp)
 
-app = Flask(__name__)
+# Route
+# api.add_resource(Hello, '/Hello')
+#
+# app = Flask(__name__)
 
+response = ''
 
-@app.route('/api',methods=['GET'])
+@app.route('/api',methods=['GET','POST'])
 def hello_world():
-    d = {}
-    d['Query'] = str(request.args['Query'])
+    global response
 
-    return jsonify(d)
-
-
+    if (request.method == 'POST'):
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        name = request_data['name']
+        response = f'HI{name}! this is python'
+        return " "
+    else:
+        return jsonify({'name':response})
 
 if __name__ == '__main__':
     app.run()
--- chehan/backend/main.py
+++ chehan/backend/main.py
@@ -4,7 +4,7 @@ import numpy as np
 with open('final_decision_Tree_model', 'rb') as f:
     model = pickle.load(f)
 
-test_data = np.array([0, 2, 5, 1, 2021])
+test_data = np.array([16, 2, 5, 1, 2021])
 answer =model.predict(test_data.reshape(1, 5))
 print(answer)
 list = answer.tolist()
