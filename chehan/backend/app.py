353827bc8074c7f5df8b3b2843f587bba86f5054
chehansivaruban <63904379+chehansivaruban@users.noreply.github.com>
2021-04-03 18:15:10 +0530

    backend edits

--- chehan/backend/app.py
+++ chehan/backend/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 import json
-
-# from flask import Blueprint
-# from flask_restful import Api
+from flask_restful import Resource,Api
 # from resources.Hello import Hello
 #
 # api_bp = Blueprint('api', __name__)
@@ -10,24 +8,45 @@ import json
 
 # Route
 # api.add_resource(Hello, '/Hello')
-#
+
 app = Flask(__name__)
+api = Api(app)
 
 response = ''
 
-
-@app.route('/api', methods=['GET', 'POST'])
-def hello_world():
-    global response
-
-    if request.method == 'POST':
+class HelloWorld(Resource):
+    def post(self):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         name = request_data['name']
         response = f'HI{name}! this is python'
-        return " "
-    else:
-        return jsonify({'name': response})
+
+    def get(self):
+        return {'name': response}
+
+class Multi(Resource):
+    def get(self,num):
+        return {'result':num*10}
+
+api.add_resource(HelloWorld,'/')
+api.add_resource(Multi,'/multi/<int:num>')
+
+
+
+
+
+# @app.route('/api', methods=['GET', 'POST'])
+# def hello_world():
+#     global response
+#
+#     if request.method == 'POST':
+#         request_data = request.data
+#         request_data = json.loads(request_data.decode('utf-8'))
+#         name = request_data['name']
+#         response = f'HI{name}! this is python'
+#         return " "
+#     else:
+#         return jsonify({'name': response})
 
 
 if __name__ == '__main__':
