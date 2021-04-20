e06178fcae231ac27b449c463eb404db28223ab2
THARUKA-D <76274228+THARUKA-D@users.noreply.github.com>
2021-04-20 09:03:17 +0530

    Deleted all files not in use for current uI

--- Front - End/NEW/map with locations/solarex/android/app/src/main/kotlin/com/example/solarex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.solarex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Front - End/NEW/map with locations/solarex/lib/widget/logged_in_widget.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class LoggedInWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-     // Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-    );
-  }
-}
