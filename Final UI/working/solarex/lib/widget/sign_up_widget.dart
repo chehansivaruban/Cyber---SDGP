a2b0dff4c5d36b32c6897fe4556ae972e1f3654f
DanuraKotte <73969786+DanuraKotte@users.noreply.github.com>
2021-04-23 03:20:49 +0530

    Final start up page

--- Final UI/working/solarex/lib/widget/sign_up_widget.dart
+++ Final UI/working/solarex/lib/widget/sign_up_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 
@@ -39,44 +40,66 @@ class SignUpWidget extends StatelessWidget {
                       return 
                     
                         Column(
-    children: [
+    children: <Widget>[
        Container(
               margin: const EdgeInsets.fromLTRB(20, 100, 20, 20),
-              height: 250,
-              child: Image(image: AssetImage("images/solarex_LOGO.png"),
+              height: 150,
+              child: Image(image: AssetImage("images/Logo.png"),
                  // height: 400,
                   width: 400,
                   fit: BoxFit.contain,),
             ),
+            Text('Continue With'),
 
-            RichText(
-                text: TextSpan(
-                    text: "Welcome to ",
-                    style:
-                    TextStyle(fontSize: 25.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                    ),
+            GoogleSignUpButtonWidget(),
 
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: "SolareX",
-                          style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,color: Colors.black)
-                      )
-                    ]
-                )
+            Text('or'),
+            Container(
+              width: constraints.maxWidth*0.8,
+              child: 
+                    TextField(
+    decoration: new InputDecoration(
+      hintText: "Username",
+      //labelText: "Email",
+      labelStyle: new TextStyle(color: const Color(0xFF424242)),
+      border: new UnderlineInputBorder(
+        borderSide: new BorderSide(
+          color: Colors.red
+        )
+      )
+    ),
             ),
-            SizedBox(height: 30.0),
+              
+              
+            ),
+
+            Container(
+              width: constraints.maxWidth*0.8,
+              child: 
+                    TextField(
+    decoration: new InputDecoration(
+      hintText: "Password",
+      //labelText: "Email",
+      labelStyle: new TextStyle(color: const Color(0xFF424242)),
+      border: new UnderlineInputBorder(
+        borderSide: new BorderSide(
+          color: Colors.red
+        )
+      )
+    ),
+            ),
+              
+              
+            ),
 
 
-            Row(
-              children: <Widget>[
 
+            Container(
 
-                Container(
-                    margin: const EdgeInsets.fromLTRB(40, 0, 10, 0),
-
-                    child: ElevatedButton(
+              alignment: Alignment.center,
+              width: constraints.maxWidth*0.8,
+              child: 
+                    ElevatedButton(
 
 
                       onPressed: () {
@@ -84,44 +107,106 @@ class SignUpWidget extends StatelessWidget {
                       },
 
                       child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                      child: Text('LOGIN', style:TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                      child: Text('Log In', style:TextStyle( fontWeight: FontWeight.bold),),
 
                       ),
 
                       style: ElevatedButton.styleFrom( shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
                       ),),
 
 
                     )
-                ),
+              
+              
+            ),
+            SizedBox(height: 20.0),
 
-                Container(
-                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            RichText(
+                text: 
 
-                    child: ElevatedButton(
-                      onPressed: () { },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
-                        child: Text('REGISTER', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold )),),
+            TextSpan(
+              children: <TextSpan>[
+                             TextSpan(text: "Don't have an account? "),
+          TextSpan(
+              text: 'Sign In',
+              style: TextStyle(color: Colors.blue),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  print('Terms of Service"');
+                }),
 
-                      style: ElevatedButton.styleFrom( shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      ),),
-
-                    )
-                ),
 
               ],
+            )
             ),
 
-            SizedBox(height: 10.0),
+         
 
-            GoogleSignUpButtonWidget(),
 
-            SizedBox(height: 30.0),
-            Text("A Project by Team Cyber", style: TextStyle(color: Colors.black,fontSize: 20.0 ),)
+  
+
+            // RichText(
+            //     text: TextSpan(
+            //         text: "Welcome to ",
+            //         style:
+            //         TextStyle(fontSize: 25.0,
+            //             fontWeight: FontWeight.bold,
+            //             color: Colors.black,
+            //         ),
+
+            //         children: <TextSpan>[
+            //           TextSpan(
+            //               text: "SolareX",
+            //               style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,color: Colors.black)
+            //           )
+            //         ]
+            //     )
+            // ),
+            // SizedBox(height: 30.0),
+
+
+            // Row(
+            //   children: <Widget>[
+
+
+            //     // Container(
+            //     //     margin: const EdgeInsets.fromLTRB(40, 0, 10, 0),
+
+            //     //     child: 
+            //     // ),
+
+            //     Container(
+            //         margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+
+            //         child: ElevatedButton(
+            //           onPressed: () { },
+            //           child: Padding(
+            //             padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+            //             child: Text('REGISTER', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold )),),
+
+            //           style: ElevatedButton.styleFrom( shape: const RoundedRectangleBorder(
+            //             borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            //           ),),
+
+            //         )
+            //     ),
+
+            //   ],
+            // ),
+
+            
+
+             Expanded(
+          child: Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: Text("A Project by Team Cyber", style: TextStyle(color: Colors.black,fontSize: 15.0 ),)
+          ),
+        ),
+
+           
+     
     ],
      
 
