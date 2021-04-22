574dbc15ff8af039246948a3cc7ad15301492fc2
DanuraKotte <danura.kottegoda@gmail.com>
2021-04-23 03:27:16 +0530

    Register page new

---
+++ Danura/lib/widget/google_sign_up_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:solarex/provider/google_sign_in.dart';
+import 'package:provider/provider.dart';
+
+class GoogleSignUpButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+    padding: EdgeInsets.all(4),
+    child:  SignInButton(
+              Buttons.Google,
+              text: "Sign up with Google",
+              onPressed: () {
+                final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+                provider.login();
+              },
+            ),
+
+  );
+  
+}
